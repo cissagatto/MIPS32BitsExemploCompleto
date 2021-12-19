7c6ee5783f94ebb2594247a0047b09616ef149e2
Elaine Cecília Gatto <elainececiliagatto@gmail.com>
2021-12-19 12:09:56 -0300

    Update exercicio.c

--- exercicio.c
+++ exercicio.c
@@ -37,7 +37,7 @@ void se_composto(int a, int b, int d, int e[]){
 	}
 }
 
-void for(int f[]){
+void loop_for(int f[]){
     for(i=0; i<10; i++) {
         som = f[i] + som;
     }
@@ -64,14 +64,8 @@ void matriz(int h[][]){
     }
 }
 
-int fib(int n){                                          
-    int i, fib1 = 1, fib2 = 1, soma;         
-    for (i = 3; i <= n; i = i + 1) {                                        
-        soma = fib1 + fib2;                    
-        fib1 = fib2;                           
-        fib2 = soma;                           
-    }                                        
-    return fib2;                             
+int fib(){                                          
+    
 } 
 
 int main() {
@@ -95,24 +89,28 @@ int main() {
 
 	switch (op) {
         case 1:
-            printf(" \n Opção selecionada: 1 - Soma");
-    		soma(f);
+            printf(" \n Opção selecionada: 1 - IF SIMPLES");
+    	    if_simples(a, b, c, e);
             break;
         case 2:
-            printf(" \n Opção selecionada: 2 - Media");
-        	media(f);
+            printf(" \n Opção selecionada: 2 - IF COMPOSTO");
+            if_composto(a, b, d, e);
             break;
         case 3:
-            printf(" \n Opção selecionada: 3 - Teste 1");
-            teste1(a,b,c,e);
+            printf(" \n Opção selecionada: 3 - FOR");
+            loop_for(f);
             break;
         case 4:
-            printf(" \n Opção selecionada: 4 - Teste 2");
-            teste2(a,b,d,e);
+            printf(" \n Opção selecionada: 4 - WHILE");
+            loop_while(g)
             break;
         case 5:
-            printf(" \n Opção selecionada: 5 - Teste 3");
-            teste3(a,b,c,e);
+            printf(" \n Opção selecionada: 5 - MATRIZ");
+            matriz(h)
+            break;
+	case 6:
+            printf(" \n Opção selecionada: 5 - RECURSÃO");
+            fib()
             break;
         default:
             printf(" \n Você digitou uma opção inválida!");
